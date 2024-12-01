1f960d272d635330528260d2c55601e6d46ad634
arnoldnekemiah <arnoldnek@gmail.com>
2024-12-01 17:39:41 +0300

    proper impelmentation od authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,26 @@
 class ApplicationController < ActionController::API
+    before_action :authenticate_user!
     before_action :configure_permitted_parameters, if: :devise_controller?
+
+    rescue_from CanCan::AccessDenied do |exception|
+        user_not_authorized
+    end
+
+    rescue_from ActiveRecord::RecordNotFound do |exception|
+        not_found
+    end
+
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name avatar])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name avatar])
+  end
+
+  def user_not_authorized
+    render json: { error: 'Unauthorized' }, status: :unauthorized
+  end
+
+  def not_found
+    render json: { error: 'Resource not found' }, status: :not_found
   end
 end
