99e46c6375495f5eb6323af7358389780c942ab4
arnoldnekemiah <arnoldnek@gmail.com>
2024-12-06 15:43:04 +0300

    fix for deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,6 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.action_dispatch.trusted_proxies = ['127.0.0.1', '::1']
 end
