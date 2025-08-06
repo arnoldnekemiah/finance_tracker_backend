e32a32b94427cef0996154179c664c28284a73ff
arnoldnekemiah <arnoldnek@gmail.com>
2025-08-06 15:48:29 +0300

    Fix migration: only add admin column, active already exists

--- db/migrate/20250806150000_add_admin_to_users.rb
+++ db/migrate/20250806150000_add_admin_to_users.rb
@@ -1,9 +1,7 @@
 class AddAdminToUsers < ActiveRecord::Migration[7.1]
   def change
     add_column :users, :admin, :boolean, default: false
-    add_column :users, :active, :boolean, default: true, null: false
     
     add_index :users, :admin
-    add_index :users, :active
   end
 end
